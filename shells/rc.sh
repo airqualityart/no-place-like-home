4cd9434ca42c1db5ab4afc00512d2f94472e6e10
Air Quality And Related Topics <45898680+airqualityart@users.noreply.github.com>
2024-11-12 21:18:05 +0100

    Updated the generic shell rc file with new aliases.

--- shells/rc.sh
+++ shells/rc.sh
@@ -6,9 +6,10 @@
 #
 # Copyright (c) 2023-now Air Quality And Related Topics.
 #
+# This code is released under the terms of the BSD 3-clause license:
+#
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met
-# (BSD "3-clause" license):
+# modification, are permitted provided that the following conditions are met:
 #
 #   (1) Redistributions of source code must retain the above copyright notice,
 #   this list of conditions and the following disclaimer.
@@ -25,11 +26,11 @@
 # MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
 # EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # We set "strict" default permissions (this should be standard, shouldn'it?)
 umask 077
@@ -40,6 +41,12 @@ alias e="emacs"
 alias re="%emacs"
 alias v="vim"
 alias gst="git status"
+alias gbr="git branch"
+alias gco="git checkout"
+
+if [ "$(uname -s)" = "Darwin" ]; then
+   alias man="LANG='en_US.UTF-8' man"
+fi
 
 # Homebrew configuration
 HOMEBREW_NO_ENV_HINTS=1
