958874f9da600bab89ddb193984429589231d857
Air Quality And Related Topics <45898680+airqualityart@users.noreply.github.com>
2025-04-26 12:08:38 +0200

    Cleaned-up the license statements.

--- emacs/main-config.el
+++ emacs/main-config.el
@@ -1,26 +1,8 @@
 ;; This is my main emacs configuration file.
 ;;
-;; Copyright (c) 2023-now Air Quality And Related Topics.
+;; Copyright (c) 2023-2025 Air Quality And Related Topics (AQArt).
 ;;
-;; Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-;; following conditions are met (BSD "3-clause" license):
-;;
-;;   (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the
-;;   following disclaimer.
-;;
-;;   (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-;;   following disclaimer in the documentation and/or other materials provided with the distribution.
-;;
-;;   (3) The name of the author may not be used to endorse or promote products derived from this software without
-;;   specific prior written permission.
-;;
-;; THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-;; TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-;; THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-;; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-;; POSSIBILITY OF SUCH DAMAGE.
+;; License: BSD 3-Clause "New" or "Revised" License (BSD-3-Clause).
 
 ;; Auto-complete and its dependencies (https://github.com/auto-complete)
 
@@ -148,7 +130,7 @@
 ;; Web modes (HTML+, CSS, JavaScript, etc.)
 
 (add-hook 'mhtml-mode-hook (lambda () (set-fill-column 119)))
-(add-hook 'js-mode-hook (lambda () (set-fill-column 119)
+(add-hook 'js-mode-hook (lambda () (set-fill-column 79)
                                    (setq ac-user-dictionary
                                          '("console"
                                            "display"
@@ -158,7 +140,7 @@
                                            "lastIndexOf"
                                            "toLowerCase"
                                            "toUpperCase"))))
-(add-hook 'css-mode-hook (lambda () (set-fill-column 119)))
+(add-hook 'css-mode-hook (lambda () (set-fill-column 79)))
 (add-to-list 'ac-modes 'mhtml-mode)
 
 ;; LaTeX mode
